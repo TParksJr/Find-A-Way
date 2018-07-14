ec1d72f74e5f56f0d778a2685bb09ace9ab4efe4
Tyson <tys_p@yahoo.ca>
2018-07-14 12:37:29 -0400

    Parkingwhiz API is functional, need to update driving cost math

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,10 @@ $(function () {
         passengers = 1,
         parkingPrice = 0,
         drivingPrice = 0,
+        parkingPrices = 0,
+        distance = 0,
+        uberXDuration = 0,
+        parkWhizAPIKey = "c58c2efc75d096c933b115d1d19e590bc31c9e21",
         uberClientID = "ojF1HzMnEg2VGCONbMWdqnjUdsDxJmsU",
         uberServerToken = "AWIcb5H_Chv5WH7cV7kyW95D-nM1Tb8n2vRbgMlG",
         uberURL = "",
@@ -24,12 +28,12 @@ $(function () {
 
 
     //variables for parking time in UNIX
-      //  var startParkingString = moment(departureTime).add(30,"minutes").format('LT')
-       // console.log(startParkingString);
-        //var startParkingNumber = parseFloat(startTimeAsUNIXString);
-        //console.log("UNIX NUMBER:"+startTimeAsUNIXNumber)
-       // var endTimeAsUNIXstring = startTimeAsUNIXNumber + 1800;
-        //console.log(endTimeAsUNIXstring);
+    //  var startParkingString = moment(departureTime).add(30,"minutes").format('LT')
+    // console.log(startParkingString);
+    //var startParkingNumber = parseFloat(startTimeAsUNIXString);
+    //console.log("UNIX NUMBER:"+startTimeAsUNIXNumber)
+    // var endTimeAsUNIXstring = startTimeAsUNIXNumber + 1800;
+    //console.log(endTimeAsUNIXstring);
 
     //initializing Firebase
     var config = {
@@ -43,39 +47,52 @@ $(function () {
     firebase.initializeApp(config);
 
     var database = firebase.database();
- 
-    
+
+
     //once lat and long have been stored, use them to do a call to the Uber API
     function uberAPICall() {
 
         //make AJAX call to Uber API
         $.get("https://api.uber.com/v1/estimates/price?start_latitude=" + startLat + "&start_longitude=" + startLng + "&end_latitude=" + endLat + "&end_longitude=" + endLng + "&server_token=" + uberServerToken, function (response) {
-        
+
             //parse data from Uber API call and converting duration from seconds to minutes
-            var distance = response.prices[5].distance, 
-                uberXDuration = response.prices[5].duration / 60,
-                uberXPrice = response.prices[5].estimate,
+                distance = response.prices[5].distance;
+                uberXDuration = response.prices[5].duration / 60;
+            var uberXPrice = response.prices[5].estimate,
                 uberPoolDuration = (response.prices[2].duration / 60) + "+",
                 uberPoolPrice = response.prices[2].estimate;
-
-            //calculate the cost of driving using constant 12.9 c/km
-            //var drivingPrice = Math.round((distance * 0.129) + parkingPrice);
-
-            //calculate the cost duration using a constant average speed of 55 km/h 
-            //var drivingDuration = Math.ceil((distance / 55) * 60);
 
             //fill in table with Uber price and duration
             $("#uberXPrice").text(uberXPrice);
             $("#uberXDuration").text(uberXDuration);
             $("#uberPoolPrice").text(uberPoolPrice);
             $("#uberPoolDuration").text(uberPoolDuration);
-            //$("#drivingPrice").text(drivingPrice);
-            //$("#drivingDuration").text(drivingDuration);
+
+            parkwhizAPICall();
+        });
+    };
+
+    function parkwhizAPICall() {
+        $.get("https://api.parkwhiz.com/v4/quotes/?q=coordinates:" + endLat + "," + endLng + "&start_time=2018-07-14T12:00&end_time=2018-07-14T13:00&api_key=" + parkWhizAPIKey, function (response) {
+            console.log(response);
+            for (i = 0; i < 3; i++) {
+                parkingPrice += parseInt(response[i].purchase_options[0].price.USD);
+            }
+            parkingPrice = parkingPrice / 3;
+        
+            //calculate the cost of driving using constant 12.9 c/km
+            var drivingPrice = Math.round((distance * 0.129) + parkingPrice);
+
+            //calculate the cost duration using a constant average speed of 55 km/h 
+            var drivingDuration = Math.ceil((distance / 55) * 60);
+
+            $("#drivingPrice").text(drivingPrice);
+            $("#drivingDuration").text(uberXDuration);
         });
     };
 
     //on click event for form submission
-    $("#submit").on("click", function(event) {
+    $("#submit").on("click", function (event) {
 
         event.preventDefault();
 
@@ -98,13 +115,13 @@ $(function () {
         $.ajax({
             url: googleURLEnd,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             endLat = response.results[0].geometry.location.lat;
             endLng = response.results[0].geometry.location.lng;
         });
 
         //make AJAX call to Google Geocode API, set to be delayed, then triggers uber API call
-        setTimeout(function() {
+        setTimeout(function () {
             $.ajax({
                 url: googleURLStart,
                 method: "GET"
@@ -112,15 +129,17 @@ $(function () {
                 startLat = response.results[0].geometry.location.lat;
                 startLng = response.results[0].geometry.location.lng;
 
-            uberAPICall();
-        });
-        database.ref().push({
-            currentLocation : currentLocation,
-            destination : destination,
-            departureTime : departureTime,
-            passengers : passengers,
-            timeAdded: firebase.database.ServerValue.TIMESTAMP
-        });
+                uberAPICall();
+            });
+            /*
+            database.ref().push({
+                currentLocation : currentLocation,
+                destination : destination,
+                departureTime : departureTime,
+                passengers : passengers,
+                timeAdded: firebase.database.ServerValue.TIMESTAMP
+            });
+            */
         }, 1000);
     });
 });
